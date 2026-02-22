0ffd921f9a098284d6e408a92c9c70a34af0735a
mseo <lemony.matthew.seo@gmail.com>
2026-02-21 23:28:03 -0800

    week 5 wednesday lab submission

--- Ackermann.cpp
+++ Ackermann.cpp
@@ -4,6 +4,14 @@ using namespace std;
 // Ackermann function definition
 unsigned long long ackermann(unsigned int m, unsigned int n) {
  // TODO: Complete the code.
+
+  if (m == 0)
+    return n+1;
+  else if (m > 0 && n == 0)
+    return ackermann(m-1,1);
+  else if (m > 0 && n > 0)
+    return ackermann(m-1,ackermann(m,n-1));
+  
 }
 
 int main() {
